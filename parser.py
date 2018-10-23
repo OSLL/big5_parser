f74bf66af770046a2915aba62d622ba7fc85e9a4
mishanyny <maykl_1999@tut.by>
2018-10-23 22:51:26 +0300

    add csv + python version

--- parser.py
+++ parser.py
@@ -1,11 +1,30 @@
+#!usr/bin/python3.7
 import requests
 from bs4 import BeautifulSoup
-import json
-
+import argparse
+import csv
 
 def set_file(res):
-    with open('result.json', 'w') as f:
-        json.dump(res, f, ensure_ascii = False)
+    colums = ['name', "расслабленность_напряженность",
+ "обособленность_привязанность", "пассивность_активность",
+ "неаккуратность_аккуратность", "отсутствие артистичности_артистичность",
+ "эмоц. устойчивость_эмоц. неустойчивость", 
+"эмоц. стабильность_эмоц. лабильность", "замкнутость_общительность",
+ "отсутствие настойчивости_настойчивость", "самодостаточность_самокритика",
+ "практичность_экспрессивность", "импульсивность_самоконтроль",
+ "подчиненность_доминирование", "нечувствительность_сензитивность",
+ "ригидность_пластичность", "непонимание_понимание", "интроверсия_экстраверсия",
+ "самоуважение_уважение других", "реалистичность_любознательность",
+ "импульсивность_самоконтроль поведения", "подозрительность_доверчивость",
+ "равнодушие_теплота", "избегание впечатлений_поиск впечатлений", "безответственность_ответственность", "соперничество_сотрудничество",
+ "эмоц. комфортность_депрессивность", "беззаботность_тревожность",
+ "избегание внимания_привлечение внимания", "консерватизм_любопытство", "беспечность_предусмотрительность"]
+    with open('result.csv', 'w') as f:
+        headers = {}
+        writer = csv.DictWriter(f, fieldnames = colums, delimiter = ';')
+        writer.writeheader()
+        writer.writerows(res)
+        
 
 
 def get_html(url):
@@ -17,7 +36,6 @@ def get_html(url):
 
 def parse_html(data, res):
     soup = BeautifulSoup(data, 'html.parser')
-    print(soup)
     row_list = soup.find('div', {'class':'stdTbl'}).find('table')
     
     for itr in row_list:
@@ -25,7 +43,6 @@ def parse_html(data, res):
             left = itr.find('td', {'class':'jFL jBF'}).find('a').string
             right = itr.find('td', {'class':'jFR jBF'}).find('a').string
             res_buf = itr.find('td', {'class':'jCell jCellF1L'}).nextSibling.string
-            print(left+'_'+right)
             res[left+'_'+right] = res_buf
         elif not ( itr.find('td', {'class':'jFL'}) == None ):
             left = itr.find('td', {'class':'jFL'}).string
@@ -34,17 +51,32 @@ def parse_html(data, res):
             res[left+'_'+right] = res_buf
             
     return res
-        
-    
-def parse(url):
-    res = {}
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-f', '--filename', required = True)
+    return parser.parse_args().filename
+
+
+def parse(url, name):
+    res = {'name' : name}
     data = get_html(url)
     res = parse_html(data, res)
-    set_file(res)
+    return res
 
+
+def main():
+    result = []
+    filename = get_args()
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter = ';')
+        next(reader)
+        for row in reader:
+            result.append(parse(row[0], row[1]))
+        set_file(result)
+        
 
 if __name__ == "__main__":
-    url = input()
-    parse(url)
+    main()
     
     
